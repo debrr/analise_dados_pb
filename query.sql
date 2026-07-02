a830efccc305dc31d77d88c955effd3329905412
debr-r <deborardrigues1@gmail.com>
2026-07-02 14:09:52 -0300

    update data

--- query.sql
+++ query.sql
@@ -1,18 +1,40 @@
+# ==========================================================
+# Sistema Nacional de Informações sobre Saneamento - SNIS
+# fonte: https://basedosdados.org/dataset/2a543ad8-3cdb-4047-9498-efe7fb8ed697?table=df7cf198-4889-4baf-bb77-4e0e28eb90ca
+# ==========================================================
+
 SELECT
-    dados.id_municipio AS id_municipio,
+    dados.ano as ano,
     diretorio_id_municipio.nome AS id_municipio_nome,
     dados.sigla_uf AS sigla_uf,
-    dados.taxa_alfabetizacao AS taxa_alfabetizacao,
-    dados.indice_envelhecimento AS indice_envelhecimento,
-    dados.idade_mediana AS idade_mediana,
-    dados.populacao_indigena AS populacao_indigena,
-    dados.populacao_indigena_terra_indigena AS populacao_indigena_terra_indigena,
-    dados.populacao_quilombola AS populacao_quilombola,
-    dados.populacao_quilombola_territorio_quilombola AS populacao_quilombola_territorio_quilombola
-FROM `basedosdados.br_ibge_censo_2022.municipio` AS dados
-LEFT JOIN (
-    SELECT DISTINCT id_municipio, nome  
-    FROM `basedosdados.br_bd_diretorios_brasil.municipio`
-) AS diretorio_id_municipio
+    diretorio_sigla_uf.nome AS sigla_uf_nome,
+    dados.populacao_atendida_agua as populacao_atendida_agua,
+    dados.populacao_atentida_esgoto as populacao_atentida_esgoto,
+    dados.populacao_urbana as populacao_urbana,
+    dados.populacao_urbana_residente_agua as populacao_urbana_residente_agua,
+    dados.populacao_urbana_atendida_agua as populacao_urbana_atendida_agua
+FROM `basedosdados.br_mdr_snis.municipio_agua_esgoto` AS dados
+LEFT JOIN (SELECT DISTINCT id_municipio,nome  FROM `basedosdados.br_bd_diretorios_brasil.municipio`) AS diretorio_id_municipio
     ON dados.id_municipio = diretorio_id_municipio.id_municipio
-WHERE dados.sigla_uf = 'PB'
+LEFT JOIN (SELECT DISTINCT sigla,nome  FROM `basedosdados.br_bd_diretorios_brasil.uf`) AS diretorio_sigla_uf
+    ON dados.sigla_uf = diretorio_sigla_uf.sigla
+    WHERE dados.sigla_uf = 'PB'
+
+
+
+# ==========================================================
+# População Brasileira - IBGE
+# fonte: https://basedosdados.org/dataset/d30222ad-7a5c-4778-a1ec-f0785371d1ca?table=0c279444-165b-41da-92cd-50fd7e66baa1
+# ==========================================================
+
+SELECT
+    dados.ano as ano,
+    dados.sigla_uf AS sigla_uf,
+    diretorio_id_municipio.nome AS id_municipio_nome,
+    dados.populacao as populacao
+FROM `basedosdados.br_ibge_populacao.municipio` AS dados
+LEFT JOIN (SELECT DISTINCT sigla,nome  FROM `basedosdados.br_bd_diretorios_brasil.uf`) AS diretorio_sigla_uf
+    ON dados.sigla_uf = diretorio_sigla_uf.sigla
+LEFT JOIN (SELECT DISTINCT id_municipio,nome  FROM `basedosdados.br_bd_diretorios_brasil.municipio`) AS diretorio_id_municipio
+    ON dados.id_municipio = diretorio_id_municipio.id_municipio
+    WHERE dados.sigla_uf = 'PB'
